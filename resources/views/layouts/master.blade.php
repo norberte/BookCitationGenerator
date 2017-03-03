3452aedb68a54b740534647ac28750e2984d2982
Andry Ramorasata <andryramorasata@alumni.ubc.ca>
2017-03-03 11:43:28 -0800

    deleted the @ inlcude(layouts/nav) and @include(layouts/footer)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,11 +19,11 @@
   <body>
 
    
-    @include('layouts/nav')
+    
     
     <div class="container">@yield('content')</div>
 
-    @include('layouts/footer')
+    
 
   
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
