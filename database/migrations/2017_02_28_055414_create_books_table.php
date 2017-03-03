466ef2b59eb9d94aadb657f9ab53273730cf4119
Norbert Eke <utherpendragon17@yahoo.com>
2017-03-02 22:44:01 -0800

    Changed the DB tables

--- database/migrations/2017_02_28_055414_create_books_table.php
+++ database/migrations/2017_02_28_055414_create_books_table.php
@@ -15,14 +15,18 @@ class CreateBooksTable extends Migration
     {
         Schema::create('Book', function (Blueprint $table) {
             $table->increments('bid');
-            if ((DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) == 'mysql') && version_compare(DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION), '5.7.8', 'ge')) {
-                $table->json('bookAttr');
-                }  else {
-                $table->text('bookAttr');
-                }
-            
-            $table->enum('fields',['foo', 'bar']);
-            $table->string('createdBy');
+            $table->json('bookAttr')->nullable();
+            $table->json('fields')->nullable();
+            $table->string('title', 255)->nullable();
+            $table->string('codeNum', 255)->nullable();
+            $table->string('authorLastName', 255)->nullable();
+            $table->string('authorFirstName', 255)->nullable();
+            $table->string('illustratorFirstName', 255)->nullable();
+            $table->string('illustratorLastName', 255)->nullable();
+            $table->string('publisher', 255)->nullable();
+            $table->string('copyright', 255)->nullable();
+            $table->string('isbn', 255)->nullable();
+            $table->string('createdBy',255)->nullable();
             $table->timestamps();
         });
     }
