8f461fe240b41e2b98a2346dad41fa62db14d736
David Smekal <david.smekal@hotmail.com>
2017-03-05 12:11:34 -0800

    I broke adding books when I created the update database method. I reverted it back.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,86 +1,88 @@
 <?php
-
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
-
 use Illuminate\Http\Request;
-
 use App\Book;
-
 class BookController extends Controller
 {
-     public function create()
+    public function create()
     {
-    	$attributes = [
-    	'title',
-    	'codeNum',
-    	'authorLastName',
-    	'authorFirstName',
-    	'illustratorFirstName',
-    	'illustratorLastName',
-    	'translatorFirstName',
-    	'translatorLastName',
-    	'publisher',
-    	'copyright',
-    	'isbn',
-    	'createdBy'];
-
-    	return view('/books/create',compact('attributes'));
-
+        $attributes = [
+            'title',
+            'codeNum',
+            'authorLastName',
+            'authorFirstName',
+            'illustratorFirstName',
+            'illustratorLastName',
+            'translatorFirstName',
+            'translatorLastName',
+            'publisher',
+            'copyright',
+            'isbn',
+            'createdBy'];
+        return view('/books/create', compact('attributes'));
     }
 
     public function store()
-    { 	
-    	
-    	// Create a new book using the request data
-    	//Save it to the database
+    {
 
+        // Create a new book using the request data
+        //Save it to the database
         DB::table('book')->insert(
+            [
+                'title' => request('title'),
+                'codeNum' => request('codeNum'),
+                'authorLastName' => request('authorLastName'),
+                'authorFirstName' => request('authorFirstName'),
+                'illustratorFirstName' => request('illustratorFirstName'),
+                'illustratorLastName' => request('illustratorLastName'),
+                'translatorFirstName' => request('translatorFirstName'),
+                'translatorLastName' => request('translatorLastName'),
+                'publisher' => request('publisher'),
+                'copyright' => request('copyright'),
+                'isbn' => request('isbn'),
+                'createdBy' => request('createdBy')]
+        );
 
-                [
-        'title' => request('title'),
-        'codeNum' => request('codeNum'),
-        'authorLastName' => request('authorLastName'),
-        'authorFirstName' => request('authorFirstName'),
-        'illustratorFirstName' => request('illustratorFirstName'),
-        'illustratorLastName' => request('illustratorLastName'),
-        'translatorFirstName' => request('translatorFirstName'),
-        'translatorLastName' => request('translatorLastName'),
-        'publisher'=> request('publisher'),
-        'copyright'=> request('copyright') ,
-        'isbn'=> request('isbn'),
-        'createdBy'=> request('createdBy')]
-            );
-    	
         //redirects to same page for now.
-         return redirect('/');
-
+        return redirect('/');
     }
-
-    // this function updates books in the database
-    public function update() {
-
-         DB::table('book')->where('bid', request('bid'))->update([
-             'title' => request('title'),
-             'codeNum' => request('codeNum'),
-             'authorLastName' => request('authorLastName'),
-             'authorFirstName' => request('authorFirstName'),
-             'illustratorFirstName' => request('illustratorFirstName'),
-             'illustratorLastName' => request('illustratorLastName'),
-             'translatorFirstName' => request('translatorFirstName'),
-             'translatorLastName' => request('translatorLastName'),
-             'publisher'=> request('publisher'),
-             'copyright'=> request('copyright'),
-             'isbn'=> request('isbn'),
-             'createdBy'=> request('createdBy')
-         ]);
-
-
-       return view('/books/edit');
-
-    }
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+//    public function populateForm(){
+//
+//
+//    }
+//
+//    public function populateFields()
+//    {
+//        $attributes = [
+//            'title',
+//            'codeNum',
+//            'authorLastName',
+//            'authorFirstName',
+//            'illustratorFirstName',
+//            'illustratorLastName',
+//            'translatorFirstName',
+//            'translatorLastName',
+//            'publisher',
+//            'copyright',
+//            'isbn',
+//            'createdBy'];
+//
+//        return view('/books/edit',compact('attributes'));
+//
+//    }
+//
+//
+//}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,36 +9,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 //routes to page for adding books
 Route::get('/books/create','BookController@create');
 
-
 //Route for storing user input into the database
 Route::post('/books','BookController@store');
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-
 // route for changing password page
 Route::get('/changePassword', 'changePassword@index');
 
-
-
 Auth::routes();
-
-//Route to the page to edit books
-Route::get('/books/edit', 'BookController@update');
-
-//Route to update edit into the database
-Route::post('/books', 'BookController@update');
-
 
