0b4d7c4fb308333f81d30d7fbe66bfb25be0f08a
Andry Ramorasata <andryramorasata@alumni.ubc.ca>
2017-03-07 17:12:10 -0800

    Your branch and 'origin/Andry' have diverged, and have 7 and 3 different commits each, respectively. 	new file:   resources/views/layouts/navbar.blade.php

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">BooKStrap</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Manage Book <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Add Book</a></li>
+                        <li><a href="#">Edit Book</a></li>
+
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Manage Template <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Add Template</a></li>
+                        <li><a href="#">Edit Template</a></li>
+
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"> Book Colletion <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Add to collecton</a></a></li>
+                        <li><a href="#">Edit Book</a></li>
+                        <li><a href="#">Export</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log Out</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
