63db3369e7bca54eb97a54381aaf1dc19807de20
Andry Ramorasata <andryramorasata@alumni.ubc.ca>
2017-03-16 14:25:58 -0700

    new file:   database/migrations/2017_03_16_032402_create_books_table.php 	new file:   database/migrations/2017_03_16_032455_create_bookcollections_table.php

---
+++ database/migrations/2017_03_16_032402_create_books_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->json('bookAttr')->nullable();
+            $table->json('fields')->nullable();
+            $table->string('title', 255)->nullable();
+            $table->string('codeNum', 255)->nullable();
+            $table->string('authorLastName', 255)->nullable();
+            $table->string('authorFirstName', 255)->nullable();
+            $table->string('illustratorFirstName', 255)->nullable();
+            $table->string('illustratorLastName', 255)->nullable();
+            $table->string('translatorFirstName', 255)->nullable();
+            $table->string('translatorLastName', 255)->nullable();
+            $table->string('publisher', 255)->nullable();
+            $table->string('copyright', 255)->nullable();
+            $table->string('isbn', 255)->nullable();
+            $table->string('createdBy',255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
