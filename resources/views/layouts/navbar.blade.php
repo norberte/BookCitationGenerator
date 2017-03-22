c38df6678208c94fb6a7158710c037797e38dceb
Andry Ramorasata <andryramorasata@alumni.ubc.ca>
2017-03-22 16:40:48 -0700

    modified:   resources/views/layouts/navbar.blade.php 	modified:   resources/views/scripts/server_processing.php 	modified:   resources/views/scripts/server_processing_templateViewer.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -33,7 +33,7 @@ html file that puts the "SEARCHBY field" in the first column this automatically 
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Manage Book <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="{{url('/books/addbook')}}">Add Book</a></li>
-          
+            <li><a href="{{url('/books/edit')}}">Edit Book</a></li>
           
           </ul>
         </li>
@@ -48,8 +48,8 @@ html file that puts the "SEARCHBY field" in the first column this automatically 
         <li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#"> Book Collection <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="{{url('/bookcollections')}}">View Collections</a></li>
-            
+            <li><a href="#">Add to collecton</a></a></li>
+            <li><a href="#">Edit Book</a></li>
             <li><a href="#">Export</a></li>
             <li><a href="{{url('/changePassword')}}">Change Password</a></li>
           </ul>
