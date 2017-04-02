1252a65d09f11e49f2f6c67287d521060d9fb7f8
David Smekal <david.smekal@hotmail.com>
2017-04-01 18:18:50 -0700

    User can search for a template.

--- resources/views/bookcollections/index.blade.php
+++ resources/views/bookcollections/index.blade.php
@@ -15,6 +15,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+
     <noscript>
       <style>
       .st-accordion ul li{
@@ -28,47 +29,22 @@
     </noscript>
     <div class="container">
         <br />
-        <h2 align="center">Search for a template</h2><br />
+        <h2 align="center">Search for a book collection</h2><br />
         <div class="form-group">
+
             <div class="input-group">
                 <span class="input-group-addon">Search</span>
-                <input type="text" name="search_text" id="search_text" placeholder="Search by template name" class="form-control" />
+                <form  action="http://localhost/bookcat/public/templates/template/search" method="post">
+                    {{ csrf_field() }}
+                    <input type="text" name="search_text" id="search_text" placeholder="Search by collection name" class="form-control"/>
+                    <input type="submit" name="search_template" value="submit">
+                </form>
+
             </div>
         </div>
         <br />
         <div id="result"></div>
     </div>
-    <script>
-        $(document).ready(function(){
-
-            load_data();
-
-            function load_data(query)
-            {
-                $.ajax({
-                    url: "templateSearch.php",
-                    method:"POST",
-                    data:{query:query},
-                    success:function(data)
-                    {
-                        $('#result').html(data);
-                    }
-                });
-            }
-            $('#search_text').keyup(function(){
-                var search = $(this).val();
-                if(search != '')
-                {
-                    load_data(search);
-                }
-                else
-                {
-                    load_data();
-                }
-            });
-        });
-    </script>
-
     <script>
           window.setTimeout(function() {
   $(".flash").fadeTo(500, 0).slideUp(500, function(){
@@ -122,8 +98,7 @@
 
                                     ?>
 
-                         
-                             <a href={{'http://localhost/bookcat/public/bookcollections/'.$collection->id}} class="btn btn-info">View Collection</a>
+
                              <form class ="form-group pull-right" action="{{'bookcollections/'.$collection->id}}" method ="post">
                              {{ csrf_field() }}
                              {{ method_field('DELETE')}}
