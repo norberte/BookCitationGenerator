c7a5f38f13b7a3c4916d3a14eb81a7ffaef4e582
Andry Ramorasata <andryramorasata@alumni.ubc.ca>
2017-04-03 11:47:43 -0700

    edited add book button to pull right and on the bottom. Added styling to edit book collection page.

--- resources/views/bookcollections/edit.blade.php
+++ resources/views/bookcollections/edit.blade.php
@@ -1,13 +1,35 @@
 @extends('layout.master')
 @section('content')
+<?php
+    echo "<p style='color:blue; font-size: 2em;'>Collection name: $collection->cname</p>";
+?>
+	<br>
+	<br>
+ <?php
+	//grabs the book collection ID of the accordion
+           $collectionId = $collection->id;
 
-	{{$collection->cname}}
-	<br>
-	<br>
-	@foreach($books as $book)
-		{{$book->title}}<br>
-		{{$book->authorFirstName}}<br>
-		
-	@endforeach
+    //gets all the books from the collection ID
+          $bookId = bookcollection::find($collectionId)->books;
+
+     //prints out all the books from the collection ID
+     foreach ($bookId as $value){
+        
+        echo "<p style='color:blue; font-size: 2em;'>Title: $value->title</p>";
+        echo "<p>&emsp;Author First Name: $value->authorFirstName</p>";
+	    echo "<p>&emsp;Author Last Name: $value->authorLastName</p>";
+	    echo "<p>&emsp;Code Number: $value->codeNum</p>";
+	    echo "<p>&emsp;Illustrator First Name: $value->illustratorFirstName</p>";
+	    echo "<p>&emsp;Illustrator Last Name: $value->illustratorLastName</p>";
+	    echo "<p>&emsp;Translator First Name: $value->translatorFirstName</p>";
+	    echo "<p>&emsp;Translator Last Name: $value->translatorLastName</p>";
+	    echo "<p>&emsp;Publisher: $value->publisher</p>";
+	    echo "<p>&emsp;Copyright: $value->copyright</p>";
+	    echo "<p>&emsp;ISBN: $value->isbn</p>";
+
+
+        echo "<br>";
+    }
+  ?>
 
 @endsection
