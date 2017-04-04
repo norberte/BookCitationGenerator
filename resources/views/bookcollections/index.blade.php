ea07672cb896e914902b14d097ea4a835ea8c759
David Smekal <david.smekal@hotmail.com>
2017-04-03 17:01:20 -0700

    Made accordian easier to read. Got rid of all the extra tags.

--- resources/views/bookcollections/index.blade.php
+++ resources/views/bookcollections/index.blade.php
@@ -81,17 +81,7 @@
                                 //prints out all the books from the collection ID
                                 foreach ($bookId as $value){
                                     echo "<p style='color:blue; font-size: 2em;'>Title: $value->title</p>";
-                                    echo "<p>&emsp;Author First Name: $value->authorFirstName</p>";
                                     echo "<p>&emsp;Author Last Name: $value->authorLastName</p>";
-                                    echo "<p>&emsp;Code Number: $value->codeNum</p>";
-                                    echo "<p>&emsp;Illustrator First Name: $value->illustratorFirstName</p>";
-                                    echo "<p>&emsp;Illustrator Last Name: $value->illustratorLastName</p>";
-                                    echo "<p>&emsp;Translator First Name: $value->translatorFirstName</p>";
-                                    echo "<p>&emsp;Translator Last Name: $value->translatorLastName</p>";
-                                    echo "<p>&emsp;Publisher: $value->publisher</p>";
-                                    echo "<p>&emsp;Copyright: $value->copyright</p>";
-                                    echo "<p>&emsp;ISBN: $value->isbn</p>";
-
 
                                     echo "<br>";
                                 }
