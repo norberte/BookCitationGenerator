40f470eac4c2dcff77ab098a6e38e06e90fcd74d
root <root@ubuntu-512mb-tor1-01.localdomain>
2017-04-05 01:17:19 +0000

    fixed the server

---
+++ public/templateview.blade.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use App\Http\Controllers;
+
+
+
+//retrieves the templatename from Javascript
+$templateName = $_POST["templatename"];
+
+
+//routes to the location where the template name will get deleted from the database
+header("Location: ". url('/templates/delete/'.$templateName));
+
+exit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
